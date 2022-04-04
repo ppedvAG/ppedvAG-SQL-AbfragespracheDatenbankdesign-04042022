7293c9f64d8fc6dac4de3196741459cd2147e0fd
AlemMCodes <93318969+AlemMCodes@users.noreply.github.com>
2022-04-04 12:06:11 +0200

    Create M002Lösung.sql

---
+++ Abfragesprache und Datenbankdesign/Abfragesprache und Datenbankdesign/M002Lösung.sql
@@ -0,0 +1,21 @@
+-- Aufgabe 1
+
+SELECT REVERSE(STUFF(REVERSE('1234567890'),1,3,'XXX'))
+
+SELECT Phone
+	  ,REVERSE(STUFF(REVERSE(Phone),1,3,'XXX')) AS Secret
+FROM Customers
+
+
+
+
+
+
+
+
+
+
+
+
+
+SELECT REPLICATE('x',LEN('1234567890')-3)
