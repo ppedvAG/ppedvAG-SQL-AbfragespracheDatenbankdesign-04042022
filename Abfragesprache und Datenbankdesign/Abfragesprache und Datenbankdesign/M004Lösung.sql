cac5dbe4247bd42e38c965b400618256c79e2794
AlemMCodes <93318969+AlemMCodes@users.noreply.github.com>
2022-04-05 11:28:21 +0200

    Update M004Lösung.sql

--- Abfragesprache und Datenbankdesign/Abfragesprache und Datenbankdesign/M004Lösung.sql
+++ Abfragesprache und Datenbankdesign/Abfragesprache und Datenbankdesign/M004Lösung.sql
@@ -12,3 +12,19 @@ WHERE City = 'Buenos Aires'
 SELECT *
 FROM Customers
 WHERE Country IN('Spain','Portugal')
+
+
+-- Aufgabe 4
+SELECT *
+FROM Products
+WHERE UnitsInStock > 100
+
+-- Aufgabe 5
+SELECT ProductID, ProductName, UnitPrice
+FROM Products
+WHERE ProductID BETWEEN 10 AND 15
+
+-- Aufgabe 6
+SELECT ProductID, ProductName, UnitPrice, SupplierID
+FROM Products
+WHERE SupplierID IN(2,7,15)
